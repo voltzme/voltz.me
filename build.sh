b2842f83c88a9c26e132a20fead67f7d601fe8ee
Vladislav Bakin <vladislav@bakin.me>
2019-07-15 18:42:52 +0300

    add correct build.sh script

--- build.sh
+++ build.sh
@@ -2,33 +2,7 @@
 
 set -e
 
-archive() {
-    if hash gtar 2>/dev/null; then
-        gtar "$@"
-    else
-        tar "$@"
-    fi
-}
+npm run build:prod -- \
+    --env.outputPath=./dist
 
-if [[ -z $1 ]]; then
-    echo "Please specify target release file"
-    exit;
-fi
-
-
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd ${BASE_DIR}
-
-rm -Rf .build && mkdir .build
-
-# backend
-if [[ ! -d "./node_modules/" ]]; then
-  npm -q install
-fi
-
-npm run -q build:prod -- \
-        --env.outputPath=.build
-
-(cd .build/ && archive -zcf $1 .)
-rm -Rf .build
+gh-pages commit -m "$(git log -1 --pretty=%B)"
