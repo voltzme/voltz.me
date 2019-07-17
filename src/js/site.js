57bd85acfdbc685dc6f1cbb0703c2ebdcc5954a1
Vladislav Bakin <vladislav@bakin.me>
2019-07-17 13:51:29 +0300

    fix 360p and 720p video change

--- src/js/site.js
+++ src/js/site.js
@@ -1,10 +1,33 @@
 import SmoothScroll from 'smooth-scroll';
 
+function domNode(tagName, attrs = {}) {
+  const el = document.createElement(tagName);
+
+  Object.entries(attrs).map(([attr, value]) => {
+    el.setAttribute(attr, value);
+  });
+
+  return el;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   new SmoothScroll('.main-intro-more', {
     speed: 500,
     updateURL: false,
   });
+
+  const video = document.querySelector('.main-intro-video');
+  if (window.innerWidth > 780) {
+    video.append(
+      domNode('source', {
+        src: require('../assets/promo_720p.mp4'),
+      }));
+  } else {
+    video.append(
+      domNode('source', {
+        src: require('../assets/promo_360p.mp4'),
+      }));
+  }
 
   document.querySelectorAll('.main-faq-item').forEach((item) => {
     const answer = item.querySelector('.main-faq-item-answer');
